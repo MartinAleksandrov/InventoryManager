c5182cbe650e306c5195234c589d9eeae41a36b8
Martin Aleksandrov <martin_20.05@abv.bg>
2024-05-14 20:13:47 +0300

    Creating controller for Products.

---
+++ InventoryManager/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿namespace InventoryManager.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}

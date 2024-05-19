5e9f391dd054815112689c21745fa2aaf6ccedf0
Martin Aleksandrov <martin_20.05@abv.bg>
2024-05-19 18:05:15 +0300

    Adding

---
+++ InventoryManager_Common/GlobalConstants/ProductConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace InventoryManager_Common.GlobalConstants
+{
+    public static class ProductConstants
+    {
+        public const int ProductNameMaxLength = 50;
+        public const int ProductNameMinLength = 3;
+
+        public const int ProductPriceMaxLength = 9999;
+        public const int ProductPriceMinLength = 1;
+
+        public const int SupplierNameMaxLength = 50;
+        public const int SupplierNameMinLength = 3;
+    }
+}
